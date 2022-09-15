3162fe8334a0adf6c9aae77ad5f4255d4ada834b
MasihNazir <masih.nazir1@gmail.com>
2022-09-15 15:38:55 +0430

    second commit

---
+++ src/componenets/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navigation from "./Navigation";
+
+
+const Header = () => {
+    return (
+        <div>
+            <Navigation />
+            <h1>Header</h1>
+        </div>
+    )
+};
+
+export default Header;
